d7d424e6dc8728c1306019915ddf1d9e1ed57095
root <selegnasol73@gmail.com>
2021-11-15 14:16:16 -0600

    Update variables.tf

--- Exercise_2/variables.tf
+++ Exercise_2/variables.tf
@@ -1,1 +1,12 @@
-# TODO: Define the variable for aws_region
+# Define the variable for aws_region
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "lambda_output_path" {
+  default = "output.zip"
+}
+
+variable "lambda_function_name" {
+  default = "greet_lambda"
+}
